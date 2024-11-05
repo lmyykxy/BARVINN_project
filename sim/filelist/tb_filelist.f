b922e450896074c3a5164438b44aa6c4c97b8fcf
lmyykxy <lmyykxy@163.com>
2024-11-05 15:32:20 +0800

    pass helloworld test

--- sim/filelist/tb_filelist.f
+++ sim/filelist/tb_filelist.f
@@ -5,9 +5,9 @@
 // /home/qingteng/Documents/Front_end/BARVINN_project/sim/src/mvu/gemv_tester_w_relu.sv
 
 
-/home/qingteng/Documents/Front_end/BARVINN_project/sim/src/mvu/gemv_tester.sv
-/home/qingteng/Documents/Front_end/BARVINN_project/sim/src/mvu/testbench_top.sv
+// /home/qingteng/Documents/Front_end/BARVINN_project/sim/src/mvu/gemv_tester.sv
+// /home/qingteng/Documents/Front_end/BARVINN_project/sim/src/mvu/testbench_top.sv
 
 // ------- riscv testbench top
 
-// /home/qingteng/Documents/Front_end/BARVINN_project/sim/src/riscv/riscv_top.v
+/home/qingteng/Documents/Front_end/BARVINN_project/sim/src/riscv/riscv_top.v
