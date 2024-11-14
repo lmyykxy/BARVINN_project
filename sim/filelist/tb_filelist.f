f749cf05476cfb885664cb18cfd483cf644daa2e
lmyykxy <lmyykxy@163.com>
2024-11-14 15:34:35 +0800

    modify syn version && finish GEMV test

--- sim/filelist/tb_filelist.f
+++ sim/filelist/tb_filelist.f
@@ -5,10 +5,10 @@
 // /home/qingteng/Documents/Front_end/BARVINN_project/sim/src/mvu/gemv_tester_w_relu.sv
 
 
-/home/qingteng/Documents/Front_end/BARVINN_project/sim/src/mvu/gemv_tester.sv
-/home/qingteng/Documents/Front_end/BARVINN_project/sim/src/mvu/testbench_top.sv
+// /home/qingteng/Documents/Front_end/BARVINN_project/sim/src/mvu/gemv_tester.sv
+// /home/qingteng/Documents/Front_end/BARVINN_project/sim/src/mvu/testbench_top.sv
 /home/qingteng/Documents/Front_end/BARVINN_project/src/mvu/usr_mem/sim_mem.v
 
 // ------- riscv testbench top
 
-// /home/qingteng/Documents/Front_end/BARVINN_project/sim/src/riscv/riscv_top.v
+/home/qingteng/Documents/Front_end/BARVINN_project/sim/src/riscv/riscv_top.v
